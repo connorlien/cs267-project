65018a56814c9f3dd99590c24a9c26e8a65ae8e6
Tejvir Jogani <tejvirjogani@gmail.com>
2022-04-23 20:46:06 -0700

    Blocking complete for direct

--- dws-blocked.c
+++ dws-blocked.c
@@ -10,8 +10,18 @@
 #define HEIGHT_BLOCK_PW 11
 #define CHANNEL_BLOCK_PW 13
 
+#define BATCH_BLOCK_DW 8
+#define CHANNEL_BLOCK_DW 2
+#define FILTER_DW 1
+#define HEIGHT_BLOCK_DW 8
+#define WIDTH_BLOCK_DW 8
+#define HEIGHT_FILTER_BLOCK_DW 3
+#define WIDTH_FILTER_BLOCK_DW 3
 
-static void dw_conv(double *X, double *F_DW, double *O, int B, int H_in, int W_in, int C_in, int H_f, int W_f, int N_dw, int H_out, int W_out, int stride_h, int stride_w)
+
+
+static void dw_conv_blocked(double *X, double *F_DW, double *O, int B, int H_in, int W_in, int C_in, int H_f, 
+    int W_f, int N_dw, int H_out, int W_out, int stride_h, int stride_w, int b_, int c_, int f_, int w_, int h_, int w_f_, int h_f_)
 {
     int mat_size = W_in * H_in;
     int f_size = W_f * H_f;
@@ -20,40 +30,75 @@ static void dw_conv(double *X, double *F_DW, double *O, int B, int H_in, int W_i
     int temp_out_img_size = W_out * H_out;
     int temp_out_size = temp_out_img_size * N_dw * C_in;
 
-    for (int b = 0; b < B; b += 1)
+    int B_b = min(BATCH_BLOCK_DW, B - b_);
+    int C_b = min(CHANNEL_BLOCK_DW, C_in - c_);
+    int F_b = min(FILTER_DW, N_dw - f_);
+    int W_b = min(WIDTH_BLOCK_DW, W_out - w_);
+    int H_b = min(HEIGHT_BLOCK_DW, H_out - h_);
+    int W_f_b = min(WIDTH_FILTER_BLOCK_DW, W_f - w_f_);
+    int H_f_b = min(HEIGHT_FILTER_BLOCK_DW, H_f - h_f_);
+
+    for (int b = 0; b < B_b; b += 1)
     {
         // PTRS TO IMG IN BATCH
-        double *curr_img = X + b * img_size;
-        double *curr_out = O + b * temp_out_size;
-        for (int c = 0; c < C_in; c += 1)
+        double *curr_img = X + (b + b_) * img_size;
+        double *curr_out = O + (b + b_) * temp_out_size;
+        for (int c = 0; c < C_b; c += 1)
         {
             // Do 2D Convolution channelwise
-            double *curr_channel = curr_img + mat_size * c;
+            double *curr_channel = curr_img + mat_size * (c + c_);
             // Filters are 2D
-            for (int f = 0; f < N_dw; f += 1)
+            for (int f = 0; f < F_b; f += 1)
             {
-                for (int w = 0; w < W_out; w += 1)
+                for (int w = 0; w < W_b; w += 1)
                 {
-                    for (int h = 0; h < H_out; h += 1)
+                    for (int h = 0; h < H_b; h += 1)
                     {
                         // MICROKERNEL - tile if needed.
-                        for (int w_f = 0; w_f < W_f; w_f += 1)
+                        for (int w_f = 0; w_f < W_f_b; w_f += 1)
                         {
-                            for (int h_f = 0; h_f < H_f; h_f += 1)
+                            for (int h_f = 0; h_f < H_f_b; h_f += 1)
                             {
                                 // PTR TO CURRENT POSITION IN FILTER
-                                double *f_curr = F_DW + f_size * (c * N_dw + f) + row_major(h_f, w_f, W_f);
+                                double *f_curr = F_DW + f_size * ((c + c_) * N_dw + (f + f_)) + row_major((h_f + h_f_), (w_f + w_f_), W_f);
 
                                 // PTR TO INPUT POSITION
-                                int h_curr = h_f + stride_h * h;
-                                int w_curr = w_f + stride_w * w;
+                                int h_curr = (h_f + h_f_) + stride_h * (h + h_);
+                                int w_curr = (w_f + w_f_) + stride_w * (w + w_);
                                 double *curr_inp = curr_channel + row_major(h_curr, w_curr, W_in);
 
                                 // PTR TO INPUT POSITION
-                                double *curr_out_xy = curr_out + temp_out_img_size * (c * N_dw + f) + row_major(h, w, W_out);
+                                double *curr_out_xy = curr_out + temp_out_img_size * ((c + c_) * N_dw + (f_ + f)) + row_major((h + h_), (w + w_), W_out);
 
                                 // CONVOLVE
                                 *curr_out_xy = *curr_out_xy + *f_curr * *curr_inp;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+static void dw_conv(double *X, double *F_DW, double *O, int B, int H_in, int W_in, int C_in, int H_f, int W_f, int N_dw, int H_out, int W_out, int stride_h, int stride_w)
+{
+    for (int b = 0; b < B; b += BATCH_BLOCK_DW)
+    {
+        for (int c = 0; c < C_in; c += CHANNEL_BLOCK_DW)
+        {
+            for (int f = 0; f < N_dw; f += FILTER_DW)
+            {
+                for (int w = 0; w < W_out; w += WIDTH_BLOCK_DW)
+                {
+                    for (int h = 0; h < H_out; h += HEIGHT_BLOCK_DW)
+                    {
+                        for (int w_f = 0; w_f < W_f; w_f += WIDTH_FILTER_BLOCK_DW)
+                        {
+                            for (int h_f = 0; h_f < H_f; h_f += HEIGHT_FILTER_BLOCK_DW)
+                            {
+                                dw_conv_blocked(X, F_DW, O, B, H_in, W_in, C_in, H_f, W_f, N_dw, H_out, W_out, stride_h, stride_w, b, c, f, w, h, w_f, h_f);
                             }
                         }
                     }
@@ -108,14 +153,7 @@ static void pw_conv(double *X, double *F_1D, double *O, int B, int H_in, int W_i
                     int H_b = min(HEIGHT_BLOCK_PW, H_in - h);
                     for (int c = 0; c < C_in; c += CHANNEL_BLOCK_PW)
                     {
-                        // double *f_curr = F_1D + f * C_in + c;
-                        // double *inp_curr = curr_img + mat_size * c + row_major(h, w, W_in);
-                        // *o_curr += (*f_curr) * (*inp_curr);
-                        
-                        
-                        
                         int C_b = min(CHANNEL_BLOCK_PW, C_in - c);
-
                         pw_blocked(B, H_in, W_in, C_in, C_out, B_b, F_b, W_b, H_b, C_b, b, f, w, h, c, F_1D, O, X);
                     }
                 }
