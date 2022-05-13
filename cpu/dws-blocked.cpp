eb42eccf50ee2ecc0de1531776f304194e4bdedb
sauravbanka <sauravbanka2000@gmail.com>
2022-05-13 00:09:06 -0700

    Blocking finally works

--- cpu/dws-blocked.cpp
+++ cpu/dws-blocked.cpp
@@ -20,6 +20,7 @@ static void dw_conv(float *X, float *F_DW, float *O, int B, int H_in, int W_in, 
 
     int temp_out_img_size = W_out * H_out;
     int temp_out_size = temp_out_img_size * N_dw * C_in;
+
     for (int b_ = 0; b_ < B; b_ += BATCH_BLOCK_DW)
     {
         int B_b = min(BATCH_BLOCK_DW, B - b_);
@@ -31,28 +32,24 @@ static void dw_conv(float *X, float *F_DW, float *O, int B, int H_in, int W_in, 
                 for (int w_ = 0; w_ < W_out; w_ += WIDTH_BLOCK_DW)
                 {
                     int W_b = min(WIDTH_BLOCK_DW, W_out - w_);
-
-                    // BLOCKING
-                    for (int b = 0; b < B_b; b += 1)
+                    for (int h_f = 0; h_f < H_f; h_f += 1)
                     {
-                        // PTRS TO IMG IN BATCH
-                        float *curr_img = X + (b + b_) * img_size;
-                        float *curr_out = O + (b + b_) * temp_out_size;
-
-                        // Do 2D Convolution channelwise
-                        float *curr_channel = curr_img + mat_size * (c_);
-                        // Filters are 2D
-
-                        for (int h = 0; h < H_b; h += 1)
+                        // MICROKERNEL - tile if needed.
+                        for (int w_f = 0; w_f < W_f; w_f += 1)
                         {
-                            for (int w = 0; w < W_b; w += 1)
+                            // BLOCKING
+                            for (int b = 0; b < B_b; b += 1)
                             {
-                                for (int h_f = 0; h_f < H_f; h_f += 1)
-                                {
-                                    // MICROKERNEL - tile if needed.
-                                    for (int w_f = 0; w_f < W_f; w_f += 1)
-                                    {
+                                // PTRS TO IMG IN BATCH
+                                float *curr_img = X + (b + b_) * img_size;
+                                float *curr_out = O + (b + b_) * temp_out_size;
+                                // Filters are 2D
 
+                                float *curr_channel = curr_img + mat_size * (c_);
+                                for (int h = 0; h < H_b; h += 1)
+                                {
+                                    for (int w = 0; w < W_b; w += 1)
+                                    {
                                         // PTR TO CURRENT POSITION IN FILTER
                                         float *f_curr = F_DW + f_size * (c_) + row_major((h_f), (w_f), W_f);
 
@@ -86,25 +83,23 @@ static void pw_conv(float *X, float *F_1D, float *O, int B, int H_in, int W_in, 
     for (int b = 0; b < B; b += BATCH_BLOCK_PW)
     {
         int B_b = min(BATCH_BLOCK_PW, B - b);
-        for (int c = 0; c < C_in; c += 1)
+        for (int h = 0; h < H_in; h += HEIGHT_BLOCK_PW)
         {
-            for (int f = 0; f < C_out; f += 1)
+            int H_b = min(HEIGHT_BLOCK_PW, H_in - h);
+            for (int w = 0; w < W_in; w += WIDTH_BLOCK_PW)
             {
-                float *f_curr = F_1D + f * C_in + c;
-                for (int h = 0; h < H_in; h += HEIGHT_BLOCK_PW)
+                int W_b = min(WIDTH_BLOCK_PW, W_in - w);
+                // Blocking
+                for (int b_ = 0; b_ < B_b; b_ += 1)
                 {
-                    int H_b = min(HEIGHT_BLOCK_PW, H_in - h);
-                    for (int w = 0; w < W_in; w += WIDTH_BLOCK_PW)
+                    float *curr_img = X + (b_ + b) * img_size;
+                    float *curr_out = O + (b_ + b) * out_size;
+                    for (int c = 0; c < C_in; c += 1)
                     {
-                        int W_b = min(WIDTH_BLOCK_PW, W_in - w);
-
-                        // Blocking
-                        for (int b_ = 0; b_ < B_b; b_ += 1)
+                        float *inp_curr = curr_img + mat_size * c;
+                        for (int f = 0; f < C_out; f += 1)
                         {
-                            float *curr_img = X + (b_ + b) * img_size;
-                            float *curr_out = O + (b_ + b) * out_size;
-
-                            float *inp_curr = curr_img + mat_size * c;
+                            float *f_curr = F_1D + f * C_in + c;
                             float *o_curr = curr_out + mat_size * f;
                             for (int h_ = 0; h_ < H_b; h_ += 1)
                             {
@@ -121,28 +116,19 @@ static void pw_conv(float *X, float *F_1D, float *O, int B, int H_in, int W_in, 
     }
 }
 
-void print_tensor(float *X, int size, const char *name)
-{
-    fprintf(stderr, "%s\n", name);
-    for (int i = 0; i < size; i += 1)
-    {
-        fprintf(stderr, "%f ", X[i]);
-    }
-    fprintf(stderr, "\n");
-}
-
 void init_conv(int bbpw, int fbpw, int wbpw, int hbpw, int cbpw, int bbdw, int cbdw, int fdw, int hbdw, int wbdw, int hfdw, int wfbdw)
 {
 
-    BATCH_BLOCK_PW = bbpw;
-    WIDTH_BLOCK_PW = wbpw;
-    HEIGHT_BLOCK_PW = hbpw;
+    BATCH_BLOCK_PW = 4;
+    WIDTH_BLOCK_PW = 150;
+    HEIGHT_BLOCK_PW = 150;
 
-    BATCH_BLOCK_DW = bbdw;
-    HEIGHT_BLOCK_DW = hbdw;
-    WIDTH_BLOCK_DW = wbdw;
+    BATCH_BLOCK_DW = 4;
+    HEIGHT_BLOCK_DW = 150;
+    WIDTH_BLOCK_DW = 150;
 
-    if (bbpw < 0 || wbpw < 0 || hbpw < 0 || bbdw < 0 || bbdw < 0 || hbdw < 0 || wbdw < 0 ) {
+    if (bbpw < 0 || wbpw < 0 || hbpw < 0 || bbdw < 0 || bbdw < 0 || hbdw < 0 || wbdw < 0)
+    {
         exit(-1);
     }
 }
